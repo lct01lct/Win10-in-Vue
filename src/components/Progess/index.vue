fc562a314d049ff9d6f823d3172d44193c3729d4
刘纯涛 <1786215856@qq.com>
2022-11-05 23:50:35 +0800

    feat: 添加了 Progess 组件的基本结构

--- src/components/Progess/index.vue
+++ src/components/Progess/index.vue
@@ -10,11 +10,31 @@
   const progessValue = ref(props.modelValue);
 
   const stepsLen = props.steps.length;
+
+  const getSectionStyles = (index: number) => ({
+    left: `${(props.width / (stepsLen - 1)) * index}px`,
+  });
+
+  const getStepStyles = (index: number) => {
+    const leftVal = (props.width / (stepsLen - 1)) * index;
+
+    return {
+      left: `${leftVal}px`,
+    };
+  };
 </script>
 
 <template>
   <div class="progess-wrapper">
     <div class="progess-line" :style="getLineStyles()">
+      <div class="progess-section" v-if="stepsLen">
+        <div
+          class="progess-section-item"
+          v-for="(item, index) in steps"
+          :key="index"
+          :style="getSectionStyles(index)"
+        ></div>
+      </div>
       <div class="progess-outer">
         <div class="progess-inner">
           <div class="slider"></div>
@@ -22,15 +42,33 @@
       </div>
     </div>
     <div class="progess-steps" v-if="showText">
-      <div class="progess-step-item" v-for="(item, index) in steps" :key="index">{{ item }}</div>
+      <div
+        class="progess-step-item"
+        v-for="(item, index) in steps"
+        :key="index"
+        :style="getStepStyles(index)"
+      >
+        {{ item }}
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
   .progess-line {
+    position: relative;
     height: 3px;
     background-color: #59595a;
+    .progess-section {
+      position: relative;
+      .progess-section-item {
+        position: absolute;
+        top: 10px;
+        width: 1px;
+        height: 4px;
+        background-color: #59595a;
+      }
+    }
 
     .progess-outer {
       width: 100%;
@@ -40,19 +78,24 @@
         position: relative;
         .slider {
           position: absolute;
+          top: -12px;
+          left: 50px;
           width: 8px;
           height: 26px;
           background-color: #0078d7;
           border-radius: 4px;
-          top: -12px;
         }
       }
     }
   }
   .progess-steps {
+    position: relative;
     display: flex;
     color: #7c7c7c;
     margin-top: 20px;
     font-size: 10px;
+    .progess-step-item {
+      position: absolute;
+    }
   }
 </style>
