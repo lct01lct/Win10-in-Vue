b3b4686090566934cf6cbc8a95fb3bf9c28fa7b1
刘纯涛 <1786215856@qq.com>
2022-11-08 18:07:10 +0800

    feat: 新增了 wifi-setting 的数据结构

--- src/view/Home/TaskBar/task-bg-item/wifi-task/wifi-list.vue
+++ src/view/Home/TaskBar/task-bg-item/wifi-task/wifi-list.vue
@@ -1,11 +1,25 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import { Icon } from '@/components';
+  import { wifiInfo } from './wifi-task';
+</script>
 
 <template>
-  <div class="wifi-list-wrapper">wifilist</div>
+  <div class="wifi-list-wrapper">
+    <div class="wifi-list-item" v-for="item in wifiInfo" :key="item.name">
+      <Icon>
+        <img v-if="item.type === '开放式'" src="@/assets/images/homePage/taskBar-img/wifi.png" />
+        <img v-else src="@/assets/images/homePage/taskBar-img/wifi-lock.png" />
+      </Icon>
+      <div class="wifi-item-info">{{ item.name }}</div>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
   .wifi-list-wrapper {
     height: 450px;
+    .wifi-list-item {
+      height: 70px;
+    }
   }
 </style>
--- src/view/Home/TaskBar/task-bg-item/wifi-task/wifi-task.ts
+++ src/view/Home/TaskBar/task-bg-item/wifi-task/wifi-task.ts
@@ -4,3 +4,15 @@ export const isSelectWifi = ref(true);
 export const isSelectFlyMode = ref(false);
 export const isSelectHotspot = ref(false);
 export const wifiStatus = computed(() => !isSelectFlyMode.value && isSelectWifi.value);
+
+interface WifiItem {
+  name: string;
+  type: '安全' | '开放式';
+}
+
+export const wifiInfo: WifiItem[] = [
+  { name: 'Nuc-student', type: '开放式' },
+  { name: 'Nuc-student-auto', type: '安全' },
+  { name: 'Nuc-teacher', type: '开放式' },
+  { name: 'Nuc-student-auto', type: '安全' },
+];
