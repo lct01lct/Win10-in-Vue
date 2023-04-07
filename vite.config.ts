556eebd9ac7d7e38b3130a18c3ebd1614cb22066
刘纯涛 <1786215856@qq.com>
2023-04-07 19:59:44 +0800

    refactor(vite.config): 修改 vite 配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import AutoImport from 'unplugin-auto-import/vite';
 import path from 'path';
+import ViteCompression from 'vite-plugin-compression';
+import { cdn, Transformed } from 'vite-plugin-cdn2';
+
 const config = loadEnv('development', './');
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+
   server: {
     port: Number(config.VITE_PORT),
   },
@@ -18,11 +23,36 @@ export default defineConfig({
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
   },
 
-  plugins: [vue(), AutoImport({ imports: ['vue'] })],
+  plugins: [
+    vue(),
+    AutoImport({ imports: ['vue'] }),
+    ViteCompression(),
+    cdn({
+      isProduction: true,
+      modules: [
+        {
+          name: 'vue',
+          global: 'Vue',
+        },
+        {
+          name: 'lodash',
+          global: 'fect',
+          spare: ['https://cdn.bootcdn.net/ajax/libs/lodash.js/4.17.21/lodash.core.js'],
+        },
+      ],
+      preset: false,
+      transform(result: Transformed & { tag: string; defer: boolean }) {
+        if (result.tag === 'script') result.defer = true;
+      },
+    }),
+  ],
 
   build: {
     outDir: './docs',
+    rollupOptions: {
+      output: {
+        manualChunks: (id: string) => (id.includes('node_modules') ? 'vendor' : undefined),
+      },
+    },
   },
-
-  base: './',
 });
