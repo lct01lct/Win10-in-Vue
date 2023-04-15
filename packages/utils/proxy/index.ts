294c92d5506b4f6fc9fd262cdf9f4016a60db0bd
刘纯涛 <1786215856@qq.com>
2023-04-15 21:42:28 +0800

    fix(utils): fix type of thisArg

--- packages/utils/proxy/index.ts
+++ packages/utils/proxy/index.ts
@@ -2,7 +2,7 @@ type Fn = (...args: any[]) => any;
 
 export const proxyFnApply = <Fun extends Fn>(
   fn: Fun,
-  cb: (originFn: Fun, thisArg, argArr: Parameters<Fun>) => void
+  cb: (originFn: Fun, thisArg: any, argArr: Parameters<Fun>) => void
 ) => {
   return new Proxy(fn, {
     apply(originFn, thisArg, argArr: Parameters<Fun>) {
