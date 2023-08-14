916c5f3208fc5c122bd7f35d180bb31e9ebed8bc
刘纯涛 <1786215856@qq.com>
2023-08-14 22:03:36 +0800

    feat(render-core): init currPointer in desktop

--- packages/render-core/src/system-app/folder/folder.ts
+++ packages/render-core/src/system-app/folder/folder.ts
@@ -1,3 +1,7 @@
 import { Pointer } from './types';
+import { Folder } from 'model-core';
 
 export const currPointer = ref<Pointer>();
+
+const desktop = Folder.getDeskTop();
+currPointer.value = desktop;
